6252930f71644c4980d490455ffab58425e9dce3
JayTi <tableros676@gmail.com>
2025-01-28 20:54:44 -0600

    Update App.js

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,8 +1,26 @@
 function agregarAmigo(){
     let amigos = [];
 
-    let entrada = document.getElementById("amigo");
-    amigos.push(entrada);
+    let entrada = document.getElementById('amigo').value;
 
-    window
+    if (entrada.trim() === ""){
+        alert("Ingrese un nombre")
+    }
+    else{
+        amigos.push(entrada);
+
+        let lista = document.getElementById("listaAmigos");
+        let lista2 = document.createElement("li");
+
+        lista2.textContent = entrada;
+
+        lista.appendChild(lista2)
+
+        document.getElementById("amigo").value = "";
+    }
+    
+
+    
+    
 }
+    
